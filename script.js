9eb81c0acc9d4c72cb1ba37aa8a9ee74c03d6d0f
Bennie, the Shepherd <118771676+benniefolyfe@users.noreply.github.com>
2023-04-10 15:57:37 -0700

    Reverting

--- script.js
+++ script.js
@@ -32,39 +32,6 @@ async function castSkill(spellId, targetId) {
   return response;
 }
 
-function showHabiticaAvatar() {
-  let html = `
-    <div id="habiticaAvatarWrapper">
-      <iframe
-        src="https://crookedneighbor.github.io/habitica-avatar/avatar.html#${UUID}"
-        scrolling="no"
-        style="width: 140px; height: 147px; overflow-y: hidden; border: none;"
-      ></iframe>
-      <p>Description: <span id="habiticaAvatarDescription"></span></p>
-      <button id="refreshHabiticaAvatar">Refresh</button>
-    </div>
-  `;
-
-  let div = document.createElement('div');
-  div.innerHTML = html;
-  div.classList.add('wrapper');
-  div.setAttribute('id', 'habiticaAvatarDiv');
-  document.getElementById('main').appendChild(div);
-
-  // Add event listener for the "Refresh" button
-  document.getElementById('refreshHabiticaAvatar').addEventListener('click', () => {
-    refreshHabiticaAvatar();
-  });
-
-  // Function to refresh the habiticaAvatar iframe
-  const refreshHabiticaAvatar = () => {
-    let iframe = document.querySelector('#habiticaAvatarDiv iframe');
-    let userId = iframe.src.split('#')[1];
-    iframe.src = `https://crookedneighbor.github.io/habitica-avatar/avatar.html#${UUID}`;
-  };
-};
-
-
 function randomAnimals(mountsObj, petsObj) {
   let html = '';
   let mounts = Object.keys(mountsObj).filter((m) => mountsObj[m]);
